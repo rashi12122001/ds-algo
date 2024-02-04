55388efa7735a51647b8ccf346a3e55993827f34
rashi arora <74211043+rashi12122001@users.noreply.github.com>
2024-02-04 09:57:08 +0530

    Update bar chart.cpp

--- arrays/bar chart.cpp
+++ arrays/bar chart.cpp
@@ -55,3 +55,38 @@ int main(){
 	}
 	return 0;
 }
+
+
+
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+ int n;
+ cin>>n;
+ int arr[n];
+ for(int i=0;i<n;i++){
+     cin>>arr[i];
+ }
+ int max=arr[0];
+ for(int i=1;i<n;i++){
+     if(arr[i]>max){
+         max=arr[i];
+     }
+     
+ }
+for(int i=max;i>0;i--){
+    for(int j=0;j<n;j++){
+        if(arr[j]>=i){
+            cout<<'*';
+        }
+        else{
+            cout<<" ";
+        }
+    }
+        cout<<endl;
+}
+
+    return 0;
+}
